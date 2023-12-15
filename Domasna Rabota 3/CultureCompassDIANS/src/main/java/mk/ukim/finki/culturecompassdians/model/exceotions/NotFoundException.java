e3b1037411e45231f864caeffc6e2cff4b2b2c4e
ElenaaAtanasoska <107962440+ElenaaAtanasoska@users.noreply.github.com>
2023-12-15 18:36:42 +0100

    refactor
    refactor

--- Domasna Rabota 3/CultureCompassDIANS/src/main/java/mk/ukim/finki/culturecompassdians/model/exceotions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.culturecompassdians.model.exceotions;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
